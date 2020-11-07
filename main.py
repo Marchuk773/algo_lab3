821cbd6776b4c9b096fa3af38231c17c35610284
Marchuk Roman <marchukroman228@gmail.com>
2020-11-08 00:59:06 +0200

    improved to linear complexity

--- main.py
+++ main.py
@@ -1,24 +1,21 @@
 def cross_product(girls_list, boys_list, tribes_counter):
     """
-    FIXME: Not optimal, find better solution if possible
-    Equation would look like this: 1*3 + 2*1 + 2*3 + 3*1
     >>> cross_product([0, 1, 2, 3], [0, 1, 0, 3], 3)
     14
     """
-    result = 0
+    duplicates_number, girls_number, boys_number = 0, 0, 0
     for i in range(1, tribes_counter + 1):
-        for j in range(1, tribes_counter + 1):
-            if i == j:
-                continue
-            result += girls_list[i] * boys_list[j]
+        girls_number += girls_list[i]
+        boys_number += boys_list[i]
+        duplicates_number += girls_list[i] * boys_list[i]
     
-    return result
+    return (girls_number * boys_number) - duplicates_number
 
 
 def find(union_list, index):
     """
-    >>> find([0, 1, 2, 4, 4, 5], 3)
-    4
+    >>> find([0, 1, 2, 4, 5, 1], 3)
+    1
     """
     if union_list[index] != index:
         union_list[index] = union_list[union_list[index]]
@@ -39,11 +36,17 @@ def solve(filename):
     >>> solve('wedd5.txt')
     30
     """
+    numbers_dictionary = {}
+    
+    def get(index):
+        return numbers_dictionary[index]
+    
     with open(filename, 'r') as file:
         
         tribes_counter = 0
         rows_number = int(file.readline())
         union_list, tribes_marker, boys_list, girls_list = [], [], [], []
+        counter = 0
         
         for i in range(rows_number + 1):
             tribes_marker.append(0)
@@ -57,11 +60,18 @@ def solve(filename):
             first_number = int(pair[0])
             second_number = int(pair[1])
             
-            if tribes_marker[first_number] == 0 and tribes_marker[second_number] == 0:
+            if first_number not in numbers_dictionary:
+                numbers_dictionary[first_number] = counter
+                counter += 1
+            if second_number not in numbers_dictionary:
+                numbers_dictionary[second_number] = counter
+                counter += 1
+            
+            if tribes_marker[get(first_number)] == 0 and tribes_marker[get(second_number)] == 0:
                 
                 tribes_counter += 1
-                tribes_marker[first_number] = tribes_counter
-                tribes_marker[second_number] = tribes_counter
+                tribes_marker[get(first_number)] = tribes_counter
+                tribes_marker[get(second_number)] = tribes_counter
                 
                 if first_number % 2 == 1:
                     boys_list[tribes_counter] += 1
@@ -73,39 +83,39 @@ def solve(filename):
                 elif second_number % 2 == 0:
                     girls_list[tribes_counter] += 1
             
-            elif tribes_marker[first_number] == 0 and tribes_marker[second_number] != 0:
+            elif tribes_marker[get(first_number)] == 0 and tribes_marker[get(second_number)] != 0:
                 
-                current_tribe = tribes_marker[second_number]
-                tribes_marker[first_number] = find(union_list, current_tribe)
+                current_tribe = find(union_list, tribes_marker[get(second_number)])
+                tribes_marker[get(first_number)] = current_tribe
                 
                 if first_number % 2 == 1:
-                    boys_list[find(union_list, current_tribe)] += 1
+                    boys_list[current_tribe] += 1
                 elif first_number % 2 == 0:
-                    girls_list[find(union_list, current_tribe)] += 1
+                    girls_list[current_tribe] += 1
             
-            elif tribes_marker[first_number] != 0 and tribes_marker[second_number] == 0:
+            elif tribes_marker[get(first_number)] != 0 and tribes_marker[get(second_number)] == 0:
                 
-                current_tribe = tribes_marker[first_number]
-                tribes_marker[second_number] = find(union_list, current_tribe)
+                current_tribe = find(union_list, tribes_marker[get(first_number)])
+                tribes_marker[get(second_number)] = current_tribe
                 
                 if second_number % 2 == 1:
-                    boys_list[find(union_list, current_tribe)] += 1
+                    boys_list[current_tribe] += 1
                 elif second_number % 2 == 0:
-                    girls_list[find(union_list, current_tribe)] += 1
+                    girls_list[current_tribe] += 1
             
-            elif tribes_marker[first_number] != 0 and tribes_marker[second_number] != 0:
+            elif tribes_marker[get(first_number)] != 0 and tribes_marker[get(second_number)] != 0:
                 
-                if tribes_marker[first_number] == tribes_marker[second_number]:
+                if tribes_marker[get(first_number)] == tribes_marker[get(second_number)]:
                     continue
                 
-                elif tribes_marker[first_number] > tribes_marker[second_number]:
-                    bigger_index = tribes_marker[first_number]
-                    smaller_index = tribes_marker[second_number]
-                    tribes_marker[second_number] = bigger_index
+                elif tribes_marker[get(first_number)] > tribes_marker[get(second_number)]:
+                    bigger_index = tribes_marker[get(first_number)]
+                    smaller_index = tribes_marker[get(second_number)]
+                    tribes_marker[get(second_number)] = bigger_index
                 else:
-                    smaller_index = tribes_marker[first_number]
-                    bigger_index = tribes_marker[second_number]
-                    tribes_marker[first_number] = bigger_index
+                    smaller_index = tribes_marker[get(first_number)]
+                    bigger_index = tribes_marker[get(second_number)]
+                    tribes_marker[get(first_number)] = bigger_index
                 
                 union_list[smaller_index] = bigger_index
                 girls_list[bigger_index] += girls_list[smaller_index]
@@ -118,5 +128,10 @@ def solve(filename):
 
 if __name__ == '__main__':
     import doctest
+    import time
     
     doctest.testmod(verbose=True)
+    
+    start_time = time.time()
+    print(f'\n{solve("wedd6.txt")} combinations for 10K unique lines')
+    print(f'10K lines file running time is {time.time() - start_time} seconds')
